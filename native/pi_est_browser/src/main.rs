98e185cb4bbf73d5bb2130dd69cc37146f7a7cc0
kenOfYugen <kenzone@gmail.com>
2017-10-24 00:25:00 +0300

    Got asm.js version working. Still can't figure out a quirk when using --manifest-path on cargo, and multiple exported functions in .cargo/config. Only one function can be exported :/

--- native/pi_est_browser/src/main.rs
+++ native/pi_est_browser/src/main.rs
@@ -1,8 +1,4 @@
 extern crate pi_est_c_wrapper;
-//pub use pi_est_c_wrapper::pi_est;
+pub use pi_est_c_wrapper::pi_est;
 
-#[no_mangle]
-pub extern "C" fn pi_est(i: i32) -> i32 {
-  4
-}
 fn main() {}
